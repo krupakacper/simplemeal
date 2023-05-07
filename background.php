e522def528ea4f120e4e976dd3e9d0846afba7ef
Kacper Krupa <krupakacper983@gmail.com>
2023-05-07 23:56:28 +0200

    Brand new files and code and concept. P.S. Background is working

---
+++ background.php
@@ -0,0 +1,161 @@
+<div class="background">
+
+      
+   <!-- Row 1 -->
+
+   <div class="bg-row">
+
+      <div class="bg-elem">
+         <div class="rotating-stuff">            
+         <div class="icon watermelon"></div>
+         <div class="brand-name"></div>
+         </div>
+      </div>
+
+      <div class="bg-elem">          
+         <div class="icon lid"></div>
+      </div>
+
+      <div class="bg-elem">
+         <div class="rotating-stuff">            
+         <div class="icon garncek"></div>
+         <div class="brand-name"></div>
+         </div>
+      </div>
+
+      <div class="bg-elem">          
+         <div class="icon baguettes"></div>
+      </div>
+
+      <div class="bg-elem">
+         <div class="rotating-stuff">            
+         <div class="icon watermelon"></div>
+         <div class="brand-name"></div>
+         </div>
+      </div>
+
+      <div class="bg-elem">          
+         <div class="icon cheese"></div>
+      </div>
+
+      <div class="bg-elem">
+         <div class="rotating-stuff">            
+         <div class="icon garncek"></div>
+         <div class="brand-name"></div>
+         </div>
+      </div>
+
+      <div class="bg-elem">          
+         <div class="icon lid"></div>
+      </div>
+
+      <div class="bg-elem">
+         <div class="rotating-stuff">            
+         <div class="icon watermelon"></div>
+         <div class="brand-name"></div>
+         </div>
+      </div>
+
+      <div class="bg-elem">          
+         <div class="icon baguettes"></div>
+      </div>
+
+      <div class="bg-elem">
+         <div class="rotating-stuff">            
+         <div class="icon garncek"></div>
+         <div class="brand-name"></div>
+         </div>
+      </div>
+
+      <div class="bg-elem">          
+         <div class="icon cheese"></div>
+      </div>
+
+      <div class="bg-elem">
+         <div class="rotating-stuff">            
+         <div class="icon watermelon"></div>
+         <div class="brand-name"></div>
+         </div>
+      </div>
+
+   </div>
+
+   
+   <!-- Row 2 -->
+   
+   <div class="bg-row">
+
+      <div class="bg-elem">          
+         <div class="icon cutlery"></div>
+      </div>
+
+      <div class="bg-elem">
+         <div class="rotating-stuff">            
+         <div class="icon burger"></div>
+         <div class="brand-name"></div>
+         </div>
+      </div>
+
+      <div class="bg-elem">          
+         <div class="icon grapes"></div>
+      </div>
+
+      <div class="bg-elem">
+         <div class="rotating-stuff">            
+         <div class="icon kuciak"></div>
+         <div class="brand-name"></div>
+         </div>
+      </div>
+
+      <div class="bg-elem">          
+         <div class="icon spatula"></div>
+      </div>
+
+      <div class="bg-elem">
+         <div class="rotating-stuff">            
+         <div class="icon burger"></div>
+         <div class="brand-name"></div>
+         </div>
+      </div>
+
+      <div class="bg-elem">          
+         <div class="icon glass"></div>
+      </div>
+
+      <div class="bg-elem">
+         <div class="rotating-stuff">            
+         <div class="icon kuciak"></div>
+         <div class="brand-name"></div>
+         </div>
+      </div>
+
+      <div class="bg-elem">          
+         <div class="icon cutlery"></div>
+      </div>
+
+      <div class="bg-elem">
+         <div class="rotating-stuff">            
+         <div class="icon burger"></div>
+         <div class="brand-name"></div>
+         </div>
+      </div>
+
+      <div class="bg-elem">          
+         <div class="icon grapes"></div>
+      </div>
+
+      <div class="bg-elem">
+         <div class="rotating-stuff">            
+         <div class="icon kuciak"></div>
+         <div class="brand-name"></div>
+         </div>
+      </div>
+
+      <div class="bg-elem">          
+         <div class="icon spatula"></div>
+      </div>
+
+   </div>
+
+
+   </div>
