6e6af7adffb4cb1638d542d5af9efea70ca99395
Kacper Krupa <krupakacper983@gmail.com>
2023-05-07 16:53:48 +0200

    Prepared basic CSS var and values. Started making rotate animation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  // const brandName = document.querySelector('.brand-name')
+
+  // function rotateText() {
+  //   brandName.innerHTML = brandName.innerText.split("").map(
+  //     (char, i) =>
+  //     `<span style="transform: rotate(${i*20})deg">${char}</span>`
+  //   ).join("")
+  // }
+
+  // rotateText()
+
   return (
-    <div>Hejunia!</div>
+    <div className="background">
+
+      <div className="bg-row">
+
+        <div className="bg-elem">
+          <div className="circle">            
+            <div className="icon"></div>
+            <div className="brand-name">
+              Simple Meal
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
   );
 }
 
